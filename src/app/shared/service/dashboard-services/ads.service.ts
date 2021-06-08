247eec0e82f21df9d3d35b9084f4a7827575dbac
youssefIbrahim <youssefibrahim315@gmail.com>
2021-06-08 08:01:19 +0200

    URL & requireOfFunction

--- src/app/shared/service/dashboard-services/ads.service.ts
+++ src/app/shared/service/dashboard-services/ads.service.ts
@@ -12,28 +12,35 @@ import { adsFilter } from '../../models/adsFilter';
 export class AdsService {
 
   _getAdsList = server.url + 'api/public/ads/nearest';
-  _getAdsType = server.url +'api/public/ads-type/list';
-  _getAdsByIdType = server.url +'api/public/ads/ad-by-Id?id=';
-  _changeStateOfAds = server.url +'api/public/ads/adActivation?';
+  _getAdsType = server.url + 'api/public/ads-type/list';
+  _getAdsByIdType = server.url + 'api/public/ads/ad-by-Id?id=';
+  _changeStateOfAds = server.url + 'api/public/ads/adActivation?';
+
+
+  _getAllAds = server.url + 'api/public/ads';   //GET  ( headers & body=>longitude=>latitude=>type )
+  _getMyAds = server.url + 'api/public/ads/myAds';  //GET ( headers & body=>page=>pageSize )
+  _getAdsFiltration = server.url + 'api/public/ads/ads-filtration'; //POST ( body )
+  _createAds = server.url + 'api/public/ads/create';    //POST ( headers & body )
 
   constructor(private _http: HttpClient) { }
 
-  changeStateOfAds(adsId,status){
-    const body={
-      id : adsId,
-      activate : status,
-     
-      
+  changeStateOfAds(adsId, status) {
+    const body = {
+      id: adsId,
+      activate: status,
+
+
     }
-    let jsonBody=JSON.stringify(body);
-    let token= JSON.parse(localStorage.getItem('currentUser'))
-    const headers= new HttpHeaders({
+    let jsonBody = JSON.stringify(body);
+    let token = JSON.parse(localStorage.getItem('currentUser'))
+    const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `${token}`
 
     })
-    console.log(token," hh",jsonBody)
-    return this._http.post(this._changeStateOfAds+'activate='+status+'&'+'id='+adsId,{},{headers:headers})}
+    console.log(token, " hh", jsonBody)
+    return this._http.post(this._changeStateOfAds + 'activate=' + status + '&' + 'id=' + adsId, {}, { headers: headers })
+  }
 
 
 
--- src/app/shared/service/dashboard-services/category.service.ts
+++ src/app/shared/service/dashboard-services/category.service.ts
@@ -14,23 +14,33 @@ export class CategoryService {
   _getcategoryList = server.url + 'api/public/ads-type/list'
   _getCategoryType = server.url + 'api/public/cat-by-adType/type='
 
-  constructor(private _http:HttpClient) {  }
 
-  getCategoryList():Observable<Icategory[]>{
+  _getPetCategory = server.url + 'api/public/pet-category'//GET  
+  _getAccCategory = server.url + 'api/public/acc-category'//GET  
+  _getAllCategory = server.url + 'api/public/category/0'//GET  
+  _getFindCategoryById = server.url + 'api/public/category/find-by-id/id=4'//GET  
+  _deleteCategory = server.url + 'api/public/category/delete?id=11' //POST  ( params-url )
+  _createCategory = server.url + 'api/public/category/create' //POST ( body )
+  _updateCategory = server.url + 'api/public/category/update' //POST ( body )  
 
-  
+
+  constructor(private _http: HttpClient) { }
+
+  getCategoryList(): Observable<Icategory[]> {
     return this._http.get<Icategory[]>(this._getcategoryList);
   }
 
-  getCategoryType(idType):Observable<Icategory[]>{
-console.log(this._getCategoryType+idType)
-    return this._http.get<Icategory[]>(this._getCategoryType+idType)
-    .pipe(
-      catchError((err) => {
-        return throwError(err.message || 'server issue ');
-      })
-    );
+  getCategoryType(idType): Observable<Icategory[]> {
+    console.log(this._getCategoryType + idType)
+    return this._http.get<Icategory[]>(this._getCategoryType + idType)
+      .pipe(
+        catchError((err) => {
+          return throwError(err.message || 'server issue ');
+        })
+      );
   }
+
+
 
 
 
