69f399e0799fbec47442116920114a5114c8c1e3
youssefIbrahim <youssefibrahim315@gmail.com>
2021-06-10 12:18:22 +0200

    setProfileData

--- src/app/components/setting/profile/profile.component.ts
+++ src/app/components/setting/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/shared/service/dashboard-services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  id
+  email
+  firstName
+  lastName
+  address
+  city
+  state
+  phone
+  username
+  country
+  emailVerified
+  avatar
+  registrationDate
+  constructor(private profileSer: ProfileService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+    this.profileSer.getProfileDetails().subscribe(
+      (data: any) => {
+        console.log(data)
+        this.id = data.response.data.id
+        this.email = data.response.data.email
+        this.firstName = data.response.data.firstName
+        this.lastName = data.response.data.lastName
+        this.address = data.response.data.address
+        this.city = data.response.data.city
+        this.state = data.response.data.state
+        this.phone = data.response.data.phone
+        this.username = data.response.data.id
+        this.country = data.response.data.country
+        this.emailVerified = data.response.data.emailVerified
+        this.avatar = data.response.data.avatar
+        this.registrationDate = data.response.data.registrationDate
+      }
+    )
+  }
 
 }
