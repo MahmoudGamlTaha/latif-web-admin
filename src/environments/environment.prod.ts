e6d8cb60b76e25e12b90c306900906821febc76c
Mahmoud <mahmoud.25490@gmail.com>
2021-08-16 20:40:16 +0200

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 };
 export const server = {
-  //url:'https://latifapp.herokuapp.com/',
- url:'http://localhost:8070/',
+  url:'https://latifapp.herokuapp.com/',
+ //url:'http://localhost:8070/',
   cloudinary_url:'https://api.cloudinary.com/v1_1/highcoder/upload',
   cloud_name:'highcoder',
   upload_presist: 'jj9igtlp'
